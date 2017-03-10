cae911b72d13b7c8dddfc06b8593a8ba9c7af697
cornu-ammonis <andrewjones232@gmail.com>
2017-03-10 18:01:12 -0500

    implemented revbits solution, pending tests

--- revBits.c
+++ revBits.c
@@ -1,18 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
 unsigned int rev (int i, int j, unsigned int A) {
 	
-	int lo = (A >> i) & 1;
-	int hi = (A >> j) & 1;
-	
+	unsigned int lo = (A >> i) & 1;
+	unsigned int hi = (A >> j) & 1;
+	unsigned int one = 1;
 	if (lo ^ hi) 
-		A ^= (lo << i) | (hi << j);
+		A ^= (one << i) | (one << j);
 	
 	return A;
 	
 	}
 unsigned int revBits(unsigned int A) {
 	
+	int n = sizeof(A) * 8;
+	
+	int i;
+	for(i = 0; i < n/2; i ++)
+		A = rev(i, n - i - 1, A);
+	
+	return A;
 }
 
 int main() {
+	
+	unsigned int a = 3;
+	printf("for 1 we get: %u", revBits(a));
 	return 1;
 }
