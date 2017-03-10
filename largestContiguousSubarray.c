559a3c68ebcd68da2ac2d496ccef5126d09059b8
cornu-ammonis <andrewjones232@gmail.com>
2017-03-10 15:10:35 -0500

    fixed bug in case where all input numbers are negative

--- largestContiguousSubarray.c
+++ largestContiguousSubarray.c
@@ -4,8 +4,9 @@
 //returns the sum of the largest contiguous subarray in nums
 int maxSubArray(int* nums, int numsSize) 
 	{
-		int currentMax = 0;
-		int tmpMax = 0;
+		
+		int currentMax = nums[0];
+		int tmpMax = nums[0];
 		int i;
 		for(i = 0; i < numsSize; i++) 
 			{
@@ -22,7 +23,7 @@ int maxSubArray(int* nums, int numsSize)
 
 int main()
 {
-	int testarr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+	int testarr[] = {-1};
 	int n = sizeof(testarr)/sizeof(testarr[0]);
 	int ans = maxSubArray(testarr, n);
 	printf("testarr answer is %d \n", ans);
