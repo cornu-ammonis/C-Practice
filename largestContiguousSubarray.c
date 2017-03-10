41f0b0f7508ea007c53f60b4184ab616a39d5648
cornu-ammonis <andrewjones232@gmail.com>
2017-03-10 15:17:47 -0500

    Fixes bugs concerning arrays with all negetive numbers -- selects largest single number

--- largestContiguousSubarray.c
+++ largestContiguousSubarray.c
@@ -8,13 +8,26 @@ int maxSubArray(int* nums, int numsSize)
 		int currentMax = nums[0];
 		int tmpMax = nums[0];
 		int i;
-		for(i = 0; i < numsSize; i++) 
+		int hasPositive = 0; //tracks whether there are only negative members
+		for(i = 0; i < numsSize; i++)
+			{
+				if (nums[i] > 0) {
+					hasPositive = 1;
+					break;}
+			}
+		for(i = 1; i < numsSize; i++) 
 			{
 				tmpMax += nums[i];
 				if (tmpMax > currentMax)
 					currentMax = tmpMax;
-				else if (tmpMax < 0)
+				else if (tmpMax < 0 && hasPositive)
 					tmpMax = 0;
+				
+				// handles case with all negative numbers
+				else if (!hasPositive){
+					if (nums[i] > currentMax) 
+						currentMax = nums[i];
+					}
 					
 			}
 		return currentMax;
@@ -23,7 +36,7 @@ int maxSubArray(int* nums, int numsSize)
 
 int main()
 {
-	int testarr[] = {-1};
+	int testarr[] = {1};
 	int n = sizeof(testarr)/sizeof(testarr[0]);
 	int ans = maxSubArray(testarr, n);
 	printf("testarr answer is %d \n", ans);
