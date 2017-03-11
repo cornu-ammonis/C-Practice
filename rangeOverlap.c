b7d2e68f7fb60f0da4811f51f11e5c9199f76ba5
cornu-ammonis <andrewjones232@gmail.com>
2017-03-11 18:45:56 -0500

    began implementation of rangeOverlap logic

--- rangeOverlap.c
+++ rangeOverlap.c
@@ -21,7 +21,7 @@ int** resizeAndCopy(int **arr, int m)
 
 //takes an int[*][n] array and the size of the first dimension, m,
 //and converts it to int** suitable for processing elsewhere in the program
-//adds one to n and intializes to 0
+//adds one to n and intializes 2nd place to 1
 int** convertToPointer(int arr[][n], int m) 
 	{
 	int **narr = (int**) malloc(m * sizeof(int*));
@@ -29,9 +29,13 @@ int** convertToPointer(int arr[][n], int m)
 	for(i = 0; i < m; i++)
 		narr[i] = (int*) calloc((n+1), sizeof(int));
 	
-	for(i = 0; i < m; i++)
-		for(j = 0; j < n; j++)
-			narr[i][j] = arr[i][j];
+	for(i = 0; i < m; i++){
+		for(j = 0; j < n; j++) {
+			narr[i][j] = arr[i][j]; }
+			
+		narr[i][2] = 1;
+		}
+		
 	
 	return narr;
 	
@@ -41,9 +45,33 @@ int computeRangeOverlap( int arr[][n], int m)
 	{
 	
 	int **narr = convertToPointer(arr, m);
+	narr = resizeAndCopy(narr, m);
 	
+	int currentPosition = m; //inclusive - narr[m] is 'empty' (actually all 0s)
+	int currentMax = 2*m; //not inclusive - while i < currentMax
 	
-	
+	int range; // which range in input element we are processing
+	int j; //iterator for dp structure
+	int incrementedDuplicate;
+	for (range = 0; range < m; range++) 
+		{
+		incrementedDuplicate = 0;
+		
+		//loop through all ranges ahead of our current range
+		for(j = range + 1; j < currentMax; j++) 
+			{
+				
+			//if they are the same range and we havent met another copy
+			//of same range, increment count and continue
+			if(narr[range][0] == narr[j][0] && narr[range][1] == narr[j][1] && !incrementedDuplicate) 
+				{
+				narr[j][2] = narr[j][2] + narr[range][2];
+				incrementedDuplicate = 1;
+				continue; 
+				}
+			
+			}
+		}
 	
 	
 	
