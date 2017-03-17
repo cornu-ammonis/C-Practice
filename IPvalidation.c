5bcc89c32d29f2f6c0b6e554e01eaebbcc6873f4
cornu-ammonis <andrewjones232@gmail.com>
2017-03-16 22:08:11 -0400

    Implemented IPv4 recursive routine and reorganized file

--- IPvalidation.c
+++ IPvalidation.c
@@ -2,45 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* validIPAddress(char* IP) 
-{
-	int len = strlen(IP); //length of input string
-	
-	if(len == 0)
-		return "Neither";
-	
-	int i;
-	int dotCount = 0; //counts periods in string
-	int colonCount = 0; //counts colons in string
-	
-	for(i = 0; i < len; i++)
-	{
-		if (IP[i] == '.')
-			dotCount++;
-		else if (IP[i] == ':')
-			colonCount++;
-	}
-	
-	//there must be exactly 3 periods xor 7 colons, and only one may be greater
-	//than 0, else return Neither as it is not a valid IP
-	if(!((dotCount == 3 ^ colonCount == 7) && (dotCount > 0 ^ colonCount > 0)))
-		return "Neither";
-	
-	if(dotCount > 0)
-		return IPv4(IP, len, 0);
-	else
-		return IPv6(IP);
-	
-	return "Neither";
-    
-}
-int IPv4(char* ip, int len, int start){
-	return 1;}
-	
-int IPv6(char* ip, int len, int start){
-	return 1;}
 
-char* IPv5(char* ip, int len, int start) 
+
+int ParseInt(const char *s,int *i)
+{
+    char *ep;
+    long l;
+
+    l=strtol(s,&ep,0);
+
+    if(*ep!=0)
+        return 0;
+
+    *i=(int)l;
+    return 1;
+ }
+
+int IPv4(char* ip, int len, int start) 
 {
 	
 	if(len == start)
@@ -62,14 +40,74 @@ char* IPv5(char* ip, int len, int start)
 		sub[j++] = ip[k];
 	}
 	sub[n] = '\0';
-	return sub;
+	
+	int* subInt = malloc(sizeof(int));
+	
+	if(!ParseInt(sub, subInt))
+	{
+		printf("Something went wrong! exiting..");
+		return 0;
+	}
+	
+	if(*subInt < 0 || *subInt > 255)
+		return 0;
+	return IPv4(ip, len, i);
+	
 	
 	
 }
 
+int IPv6(char* ip, int len, int start){
+	return 1;}
+
+char* validIPAddress(char* IP) 
+{
+	int len = strlen(IP); //length of input string
+	
+	if(len == 0)
+		return "Neither";
+	
+	int i;
+	int dotCount = 0; //counts periods in string
+	int colonCount = 0; //counts colons in string
+	
+	for(i = 0; i < len; i++)
+	{
+		if (IP[i] == '.')
+			dotCount++;
+		else if (IP[i] == ':')
+			colonCount++;
+	}
+	
+	//there must be exactly 3 periods xor 7 colons, and only one may be greater
+	//than 0, else return Neither as it is not a valid IP
+	if(!(((dotCount == 3) ^ (colonCount == 7)) && ((dotCount > 0) ^ (colonCount > 0))))
+		return "Neither";
+	
+	if(dotCount > 0)
+		return IPv4(IP, len, 0);
+	else
+		return IPv6(IP, len, 0);
+	
+	return "Neither";
+    
+}
+
+
+
+	
+
+
+
+
 int main(void) 
 {
-	char* test = "11.12.1.11.1";
-	printf(IPv5(test, strlen(test), 0));
+	char* test = "255.255.255.255.255";
+	if(IPv4(test, strlen(test), 0))
+		printf("valid!");
+	else
+		printf("Invalid or error!");
 	return 1;
 }
+
+
